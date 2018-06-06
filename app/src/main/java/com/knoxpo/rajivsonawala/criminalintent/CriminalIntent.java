f74adacbb4a1bbb69aaa5fbb64543241b9d4f1de
rajSon5512 <rjvsonawala@gmail.com>
2018-06-05 23:20:07 -0700

    First Commit

---
+++ app/src/main/java/com/knoxpo/rajivsonawala/criminalintent/CriminalIntent.java
@@ -0,0 +1,13 @@
+package com.knoxpo.rajivsonawala.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriminalIntent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criminal_intent);
+    }
+}
